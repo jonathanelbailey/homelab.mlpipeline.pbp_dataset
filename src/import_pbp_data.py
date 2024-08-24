2662d4ffd0d3245e7742b9141a751419735b65c0
Jonathan Bailey <bailey.jonathan@magiccityit.com>
2024-08-24 14:28:33 -0500

    fix main

--- src/import_pbp_data.py
+++ src/import_pbp_data.py
@@ -2,30 +2,11 @@ import datetime
 import os
 
 from src.data_utils import clean_nfl_data, load_pbp_data, process_dataframe, update_dataset
+import argparse
+from textwrap import dedent
 
 
-def main(args):
-    years = list(range(args.start_year, args.end_year + 1, 1))
-    file_prefix = "pbp_data"
-    os.makedirs(file_prefix, exist_ok=True)
-    print(f"Importing Play-by-Play Data for the following years:\n{years}")
-    pbp_df = load_pbp_data(years)
-    print(f"Cleaning Play-by-Play Data")
-    pbp_data = clean_nfl_data(pbp_df)
-    print(f"Saving Play-by-Play Data to CSV")
-    process_dataframe(pbp_data, file_prefix)
-    update_dataset(dataset_name=args.dataset, dataset_project=args.project, file_prefix=file_prefix)
-
-
-def get_current_season_year():
-    # return datetime.datetime.now().year
-    return 2023
-
-
-if __name__ == "__main__":
-    import argparse
-    from textwrap import dedent
-
+def main():
     parser = argparse.ArgumentParser(
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description="Get Play-by-Play Data with nfl_data_py",
@@ -45,7 +26,8 @@ if __name__ == "__main__":
 
     # Add arguments for all hyperparameters
     parser.add_argument(
-        "-s", "--start-year", type=int, metavar=year_metavar, required=False, default=1999, help="Starting Season.  Default: 1999"
+        "-s", "--start-year", type=int, metavar=year_metavar, required=False, default=1999,
+        help="Starting Season.  Default: 1999"
     )
     parser.add_argument(
         "-e",
@@ -66,8 +48,27 @@ if __name__ == "__main__":
         help="Dataset Name.  Default: NFL Play-by-Play Data",
     )
     parser.add_argument(
-        "-p", "--project", type=str, metavar="PROJECT_NAME", required=False, default="NFL Models", help="Project Name.  Default: NFL Models"
+        "-p", "--project", type=str, metavar="PROJECT_NAME", required=False, default="NFL Models",
+        help="Project Name.  Default: NFL Models"
     )
 
     args = parser.parse_args()
-    main(args)
+    import_pbp_data(args)
+
+
+def import_pbp_data(args):
+    years = list(range(args.start_year, args.end_year + 1, 1))
+    file_prefix = "pbp_data"
+    os.makedirs(file_prefix, exist_ok=True)
+    print(f"Importing Play-by-Play Data for the following years:\n{years}")
+    pbp_df = load_pbp_data(years)
+    print(f"Cleaning Play-by-Play Data")
+    pbp_data = clean_nfl_data(pbp_df)
+    print(f"Saving Play-by-Play Data to CSV")
+    process_dataframe(pbp_data, file_prefix)
+    update_dataset(dataset_name=args.dataset, dataset_project=args.project, file_prefix=file_prefix)
+
+
+def get_current_season_year():
+    # return datetime.datetime.now().year
+    return 2023
